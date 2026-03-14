6bec4a2fdcc4b67a49454c13d07f5e8c83020b3d
Chrxs-21 <christianxdjrc@gmail.com>
2026-03-13 21:53:11 -0400

    feat: Creación main

---
+++ FilesystemSimulator/src/filesystem/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package filesystem;
+
+/**
+ *
+ * @author Chris
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("FileSystem Simulator arrancando...");
+    }
+}
